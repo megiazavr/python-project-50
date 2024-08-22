a2adc3d2db527d8108b6e2d65b3174424340b7f0
megiazavr <milena.egiazaryan77@mail.ru>
2024-08-22 13:23:49 +0100

    add gendiff -h

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,22 @@
 import argparse
 
-
-def main():
+def parser_file():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
     parser.add_argument('first_file', help='First file to compare')
     parser.add_argument('second_file', help='Second file to compare')
+    parser.add_argument(
+        '-f',
+        '--format',
+        default='stylish',
+        help='set format of output',    
+    )
     args = parser.parse_args()
+
+
+def main():
+    parser_file()
 
 if __name__ == "__main__":
     main()
