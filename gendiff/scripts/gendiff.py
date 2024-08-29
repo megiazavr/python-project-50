847eab0f63a4b8971dfd045a77d606f9ad705a5a
megiazavr <milena.egiazaryan77@mail.ru>
2024-08-29 17:46:31 +0100

    json difference

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,23 +1,13 @@
-import argparse
-
-def parser_file():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('first_file', help='First file to compare')
-    parser.add_argument('second_file', help='Second file to compare')
-    parser.add_argument(
-        '-f',
-        '--format',
-        default='stylish',
-        help='set format of output',    
-    )
-    args = parser.parse_args()
+#!usr/bin/env python3
+from gendiff import parse_arguments, generate_diff
 
 
 def main():
-    parser_file()
+    args = parse_arguments()
+    print(generate_diff(args.first_file,
+                        args.second_file,
+                        formatter=args.format))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-
