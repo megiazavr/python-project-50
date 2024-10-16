93bc9cea97e62667920ff6dc32bce090bca87566
megiazavr <milena.egiazaryan77@mail.ru>
2024-10-16 17:15:55 +0100

    change stylish.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,31 +1,53 @@
-from gendiff.formatters.string import string
+def gen_plain_format(diff, path=''):
+    """
+    Returns a string in plain format
+
+    Args:
+        diff (list): List with differences
+        path (str, optional): Path to current element. Defaults to ''.
+    """
+    result_string = ''
+
+    for value in diff:
+
+        path += f'{value["key"]}.'
+        if value.get('status') == 'nested':
+            result_string += f'{gen_plain_format(value.get('value'), path)}\n'
+
+        elif value.get('status') == 'added':
+            result_string += (f"Property '{path.strip('.')}' "
+                              f"was added with value: "
+                              f"{to_str(value['value'])}\n")
+
+        elif value.get('status') == 'deleted':
+            result_string += f"Property '{path.strip('.')}' was removed\n"
+
+        elif value.get('status') == 'changed':
+            result_string += (f"Property '{path.strip('.')}' was updated. "
+                              f"From {to_str(value['old_value'])} "
+                              f"to {to_str(value['new_value'])}\n")
+
+        path = path[0:-len(value['key']) - 1]
+
+    return result_string.strip('\n')
 
 
-def upgrade_str(value):
-    if isinstance(value, dict):
+def to_str(value):
+    """
+    Converts value to string in plain format
+
+    Args:
+        value (bool, int, list, dict): Value to convert
+    """
+    if isinstance(value, list) or isinstance(value, dict):
         return '[complex value]'
-    new_value = repr(value) if isinstance(value, str) else value
-    return formater_string(new_value)
-
-
-def formater_plain(diff):
-    def build_diff(diff, parent=''):
-        result = []
-        for node in diff:
-            action_type, key = node.get('action_type'), node.get('key')
-            value, new_value = node.get("value"), node.get('new_value')
-            full_key = f"{parent}.{key}" if parent else key
-            if action_type == 'children':
-                result.extend(build_diff(value, full_key))
-            elif action_type == 'added':
-                result.append(f"Property '{full_key}' was added with 
-value: {upgrade_str(value)}")
-            elif action_type == 'removed':
-                result.append(f"Property '{full_key}' was removed")
-            elif action_type == 'changed':
-                result.append(
-                    f"Property '{full_key}' was updated. From 
-{upgrade_str(value)} to {upgrade_str(new_value)}"
-                )
-        return result
-    return build_diff(diff)
+    elif value is None:
+        return 'null'
+    elif value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif isinstance(value, int):
+        return value
+    else:
+        return f"'{value}'"
