614f9bb23bb39c4aeeb25f81c2e0523b65207e73
megiazavr <milena.egiazaryan77@mail.ru>
2024-10-16 17:52:56 +0100

    changes

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,68 +1,71 @@
-from gendiff.scripts.gendiff import generate_diff
-from gendiff.parser_file import parser_file
+import pytest
+from gendiff import generate_diff
 
 
-def test_generate_diff1():
-    file1 = 'file1.json'
-    file2 = 'file2.json'
-    correct_result = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
-    assert generate_diff(file1, file2) == correct_result
+def read_result(path_file):
+    with open(path_file, "r") as file:
+        return file.read()
 
 
-def test_generate_diff2():
-    file1 = 'file1.yml'
-    file2 = 'file2.yml'
-    correct_result = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
-    assert generate_diff(file1, file2) == correct_result
+@pytest.mark.parametrize(
+    "file1, file2, formatter, result",
+    [
+        (
+            "tests/fixtures/flat_file1.json",
+            "tests/fixtures/flat_file2.yml",
+            "stylish",
+            "tests/fixtures/stylish_flat_string.txt",
+        ),
+        (
+            "tests/fixtures/nested_file3.json",
+            "tests/fixtures/nested_file4.yml",
+            "stylish",
+            "tests/fixtures/stylish_nested_string.txt",
+        ),
+        (
+            "tests/fixtures/flat_file1.json",
+            "tests/fixtures/flat_file2.yml",
+            "plain",
+            "tests/fixtures/plain_flat_string.txt",
+        ),
+        (
+            "tests/fixtures/nested_file3.json",
+            "tests/fixtures/nested_file4.yml",
+            "plain",
+            "tests/fixtures/plain_nested_string.txt",
+        ),
+        (
+            "tests/fixtures/nested_file3.json",
+            "tests/fixtures/nested_file4.yml",
+            "json",
+            "tests/fixtures/json_string.txt",
+        ),
+    ],
+)
+def test_generate_diff_formatters(file1, file2, formatter, result):
+    assert generate_diff(file1, file2, formatter) == read_result(result)
 
 
-def test_generate_diff3():
-    file1 = 'file4.json'
-    file2 = 'file5.json'
-    correct_result = parser_file('right_test_gendiff.txt')
-    assert generate_diff(file1, file2) == correct_result
+def test_invalid_format_file():
+    with pytest.raises(
+        ValueError,
+        match=("Unsupported file format. "
+               "Expected '.yaml', '.yml' or '.json'."),
+    ):
+        generate_diff(
+            "tests/fixtures/flat_file1.txt",
+            "tests/fixtures/flat_file2.yml",
+            "stylish"
+        )
 
 
-def test_generate_diff4():
-    file1 = 'file4.yml'
-    file2 = 'file5.yaml'
-    correct_result = parser_file('right_test_gendiff.txt')
-    assert generate_diff(file1, file2) == correct_result
-
-
-def test_generate_diff5():
-    correct_result = '''Property 'common.follow' was added with value: 
-false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]'''
-    assert generate_diff('file4.yml', 'file5.json', 'plain') == 
-correct_result
-
-
-def test_generate_diff6():
-    file1 = 'file4.yml'
-    file2 = 'file5.json'
-    correct_result = parser_file('right_test_json.txt')
-    assert generate_diff(file1, file2, 'json') == correct_result
+def test_invalid_formatter():
+    with pytest.raises(
+        ValueError,
+        match=("Unsupported format. Expected 'stylish', 'plain' or 'json'"),
+    ):
+        generate_diff(
+            "tests/fixtures/flat_file1.json",
+            "tests/fixtures/flat_file2.yml",
+            "nested"
+        )
