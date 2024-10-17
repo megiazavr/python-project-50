cc6375d754319292afb3fe00e8b188e0750a6db0
megiazavr <milena.egiazaryan77@mail.ru>
2024-10-17 13:14:08 +0100

    add autopep8

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,7 +1,7 @@
 def builder(file1, file2):
     """
-    Builds diff between two dicts and returns it as a list of dicts with the following 
-structure {'key': 'key', 'value': 'value', 'status': 
+    Builds diff between two dicts and returns it as a list of dicts with the following
+structure {'key': 'key', 'value': 'value', 'status':
 'status'}
 
     Args:
